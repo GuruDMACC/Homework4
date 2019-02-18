4929a371b25571b9d263ee18bb4bced5308f03e7
Guru <grmadamsetty@dmacc.edu>
2019-02-18 00:58:42 -0600

    InitialCommit

---
+++ hw4/src/edu/dmacc/codedsm/hw4/VariableAdder.java
@@ -0,0 +1,21 @@
+package edu.dmacc.codedsm.hw4;
+
+public class VariableAdder {
+
+	final static int con=9;
+	 static int var=1;
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 int metVar=2;
+	 int result=0;
+	 
+	 result=con+var+metVar;
+
+	 System.out.println("sum of "+con+ " "+ var+ " " + metVar+ " is :" + result);
+	 
+	 
+	}
+
+}
